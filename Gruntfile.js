46e436cbc621d523ae19a8ce44ffe287917262f9
Jesus Velasco <jesus.velasco@babel.com>
2016-10-16 13:50:45 +0200

    Inicio de la estructura del proyecto

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt){
+    //Configuración de Grunt
+    var settings = {
+        less:{
+            style:{
+                files:{//archivos a compilar
+                "css/style.css":"css/less/style.less" //destino:origen
+             }
+            }
+        },
+          watch:{
+              styles:{
+                  files:["less/*.less"], //Observa cualquier cambio en archivo less
+                  tasks:["less"], //Ejecuta la compilación CSS
+                  options:{
+                    spawn: false
+                 }
+              }
+          }
+       
+    };
+    //Cargamos la configuración de Grunt
+    grunt.initConfig(settings);
+    //Cargamos plugins
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    //Definimos tareas disponibles para grunt-cli
+    grunt.registerTask('default',['less','watch']);
+    grunt.registerTask('poduction',['less']);
+}
